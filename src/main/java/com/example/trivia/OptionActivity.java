e01286d8a293e0fd4de745a7b468bf672153b677
AshuJoshi001 <47688714+AshuJoshi001@users.noreply.github.com>
2021-06-17 20:25:55 +0530

    Add files via upload

---
+++ src/main/java/com/example/trivia/OptionActivity.java
@@ -0,0 +1,25 @@
+package com.example.trivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OptionActivity extends AppCompatActivity {
+    private Button gotobutton;
+
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((int) R.layout.activity_option);
+        Button button = (Button) findViewById(R.id.go_to_quiz);
+        this.gotobutton = button;
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                OptionActivity.this.startActivity(new Intent(OptionActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
